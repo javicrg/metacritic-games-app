faf60e7ed3389a9beb91b02dcfd814f6c92a7ef1
Javi <jcareagamoraleda@gmail.com>
2026-07-29 11:50:37 +0200

    chore: integrate Prettier with ESLint and auto-format code

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
-import {useState} from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
+import { useState } from "react";
 
-
-const icon = require("./assets/icon.png")
+const icon = require("./assets/icon.png");
 
 export default function App() {
   const [timesPressed, setTimesPressed] = useState(0);
 
   return (
     <View style={styles.container}>
-      <StatusBar style="light"/>
+      <StatusBar style="light" />
 
-        <Pressable
-          onPress={() => {
-            setTimesPressed(current => current + 1);
-          }}
-          style={({pressed}) => [
-            {
-              backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
-            },
-            styles.wrapperCustom,
-          ]}>
-          {({pressed}) => (
-            <Text style={{fontWeight: pressed ? 'bold' : 'normal'}}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
-          )}
-        </Pressable>
+      <Pressable
+        onPress={() => {
+          setTimesPressed((current) => current + 1);
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+          },
+          styles.wrapperCustom,
+        ]}
+      >
+        {({ pressed }) => (
+          <Text style={{ fontWeight: pressed ? "bold" : "normal" }}>
+            {pressed ? "Pressed!" : "Press Me"}
+          </Text>
+        )}
+      </Pressable>
     </View>
   );
 }
@@ -33,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
